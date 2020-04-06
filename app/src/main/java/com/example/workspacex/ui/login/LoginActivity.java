cbf88fce6c486008f383a7eddeae609e8d3b2070
yomna <yomna.eldawy@bosta.co>
2020-04-06 02:17:38 +0200

    sign up and in

--- app/src/main/java/com/example/workspacex/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/workspacex/ui/login/LoginActivity.java
@@ -25,6 +25,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -39,6 +40,7 @@ import com.example.workspacex.ui.login.LoginViewModel;
 import com.example.workspacex.ui.login.LoginViewModelFactory;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
@@ -49,6 +51,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -70,50 +73,57 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 
-    public void handleSignUp (View view) {
-        EditText emailET = findViewById(R.id.editTextEmail);
-        String email = emailET.getText().toString();
-        EditText pwdET = findViewById(R.id.editTextPassword);
-        String pwd = pwdET.getText().toString();
+    public void handleSignUp (View view) throws JSONException {
+        EditText emailET = findViewById(R.id.editTextEmail_r);
+        final String email = emailET.getText().toString();
+        EditText pwdET = findViewById(R.id.editTextPassword_r);
+        final String pwd = pwdET.getText().toString();
         EditText firstNameET = findViewById(R.id.editTextFirstName);
-        String firstName = firstNameET.getText().toString();
+        final String firstName = firstNameET.getText().toString();
         EditText lastNameET = findViewById(R.id.editTextLastName);
-        String lastName = lastNameET.getText().toString();
-        RequestQueue requestQueue;
-        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        final String lastName = lastNameET.getText().toString();
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         String url ="http://192.168.43.102:5000/customer/signup";
-        HashMap<String, String> params = new HashMap<String, String>();
-        params.put("email", email);
-        params.put("pwd", pwd);
-        params.put("fn", firstName);
-        params.put("ln", lastName);
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
-                url, new JSONObject(params), new Response.Listener<JSONObject>()
-        {
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("email", email);
+        jsonBody.put("password", pwd);
+        jsonBody.put("fn", firstName);
+        jsonBody.put("ln", lastName);
 
+        JsonObjectRequest jsonOblect = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                System.out.println("Response is: " + response);
+                try {
+                    if ((boolean) response.get("success") == true) {
+                        Toast.makeText(getApplicationContext(), "Successfully registered. please sign in", Toast.LENGTH_LONG).show();
+                        switchToLogin(findViewById(R.id.editTextEmail_r));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Error signing up", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-
         }, new Response.ErrorListener() {
-
             @Override
             public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+                System.out.println(error);
             }
-        })
-        {
+        }) {
             @Override
-            public String getBodyContentType()
-            {
-                return "application/json";
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                final Map<String, String> headers = new HashMap<>();
+                return headers;
             }
         };
-        requestQueue.add(jsonObjReq);
+        Volley.newRequestQueue(this).add(jsonOblect);
+
+        // Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_LONG).show();
+
     }
 
-    public  void switchToSignup (View view) {
+
+    public void switchToSignup (View view) {
         View l = findViewById(R.id.login_layout);
         l.setVisibility(View.GONE);
         View reg = findViewById(R.id.register_layout);
@@ -125,38 +135,46 @@ public class LoginActivity extends AppCompatActivity {
         View reg = findViewById(R.id.login_layout);
         reg.setVisibility(View.VISIBLE);
     }
-    public void handleSignIn(View view) throws IOException {
-        int SDK_INT = android.os.Build.VERSION.SDK_INT;
-        if (SDK_INT > 8)
-        {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                    .permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            //your codes here
-            RequestQueue queue = Volley.newRequestQueue(this);
-            String url ="http://192.168.43.102:5000/staff";
+    public void handleSignIn(View view) throws IOException, JSONException {
+        EditText emailET = findViewById(R.id.editTextEmail);
+        final String email = emailET.getText().toString();
+        EditText pwdET = findViewById(R.id.editTextPassword);
+        final String pwd = pwdET.getText().toString();
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        String url ="http://192.168.43.102:5000/customer/login";
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("email", email);
+        jsonBody.put("password", pwd);
 
-// Request a string response from the provided URL.
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        System.out.println(response);
+        JsonObjectRequest jsonOblect = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if ((boolean) response.get("success") == true) {
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Wrong email or password", Toast.LENGTH_LONG).show();
                     }
-                }, new Response.ErrorListener() {
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
+                System.out.println(error);
             }
-        });
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                final Map<String, String> headers = new HashMap<>();
+                return headers;
+            }
+        };
+        Volley.newRequestQueue(this).add(jsonOblect);
 
-// Add the request to the RequestQueue.
-            queue.add(stringRequest);
 
-        }
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
         // Instantiate the RequestQueue
 
 
