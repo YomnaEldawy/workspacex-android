a4bf12984cc72272a3bdbc5240486f647f758d40
yomna <yomna.eldawy@bosta.co>
2020-05-11 01:26:32 +0200

    codes merged

--- app/src/main/java/com/example/workspacex/config/ServerIP.java
+++ app/src/main/java/com/example/workspacex/config/ServerIP.java
@@ -1,7 +1,7 @@
 package com.example.workspacex.config;
 
 public class ServerIP {
-    private static String IP = "http://192.168.1.10:5000";
+    private static String IP = "http://192.168.1.104:5000";
     public static String getIP() {
         return IP;
     }
